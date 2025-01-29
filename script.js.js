08639351967209e1f06b46c26602f7e8cc2b3e49
tamro killas <t.kilasonia2006@gmail.com>
2025-01-29 23:14:08 +0400

    Initial commit

---
+++ script.js.js
@@ -0,0 +1,23 @@
+// script.js
+
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = '';
+}
+
+function deleteLast() {
+    let currentDisplay = document.getElementById("display").value;
+    document.getElementById("display").value = currentDisplay.slice(0, -1);
+}
+
+function calculate() {
+    let expression = document.getElementById("display").value;
+    try {
+        document.getElementById("display").value = eval(expression);
+    } catch (error) {
+        document.getElementById("display").value = 'Error';
+    }
+}
